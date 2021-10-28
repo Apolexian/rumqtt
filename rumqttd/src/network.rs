5a20b66a1a464d75ff94d0552531442df2741a20
Apolexian <ivan@niktivan.org>
2021-10-28 20:06:34 +0100

    Adjust indexing to try expand BytesMut buffer

--- rumqttd/src/network.rs
+++ rumqttd/src/network.rs
@@ -165,7 +165,7 @@ impl Network {
             Err(e) => return Err(io::Error::new(io::ErrorKind::InvalidData, e.to_string())),
         };
 
-        self.socket.send(&mut write[..]).await;
+        self.socket.send(&mut write[..len]).await;
         Ok(len)
     }
 
@@ -176,7 +176,7 @@ impl Network {
             Err(e) => return Err(io::Error::new(io::ErrorKind::InvalidData, e.to_string())),
         };
 
-        self.socket.send(&mut write[..]).await;
+        self.socket.send(&mut write[..len]).await;
         Ok(len)
     }
 
